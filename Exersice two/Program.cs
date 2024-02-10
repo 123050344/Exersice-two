8015cf5b96696805fcf031cc408eacf23ac6027c
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-02-10 16:14:47 -0600

    Agregar archivos de proyecto.

---
+++ Exersice two/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("The area of a triangle");
+int Base, height;
+Console.WriteLine("Write a base: ");
+Base = int.Parse (Console.ReadLine ());
+
+Console.WriteLine("Write a height: ");
+height = int.Parse (Console.ReadLine ());
+
+int area;
+area= Base *  height / 2;
+
+Console.WriteLine("You area is: " + area);
+Console.WriteLine("Thanks!!");
+Console.ReadLine ();
